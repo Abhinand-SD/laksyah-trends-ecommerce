c8b1c57366ba882d4e7e8bba2d8f5f66ab0a198a
Abhinand SD <abhinandssd90@gmail.com>
2025-11-14 15:55:21 +0530

    start contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact
+    <div>
+
+    <div className='text-center text-2xl pt-10 border-t'>
+      <Title text1={'CONTACT'} text2={'US'}/>
+    </div>
+
+    <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+      <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt=''/>
+
+      <div className='flex flex-col justify-center items-start gap-6'>
+        <p className='font-semibold text-xl text-gray-600'>Our store</p>
+        <p className='text-gray-500'>Abhinand <br/> suite 390, whashingtome</p>
+        <p className='text-gray-500'>Tel :34987897432 <br/> Email: asdsad@gmail.com</p>
+        <p className='font-semibold text-xl text-gray-600'>Lalshaya store</p>
+        <p className='text-gray-500'>Abhinand <br/> suite 390, whashingtome</p>
+        <p></p>
+      </div>
+    </div>
       
     </div>
   )
